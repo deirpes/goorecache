5870a52ee28d32df7441496a0aeb08812450ad24
Chien Nguyen <deirpes@gmail.com>
2021-06-18 11:14:58 +0700

    Added log

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ module.exports = function (mongoose, option) {
             return exec.apply(this, arguments);
         }
         const key = this._key || Hash.md5(JSON.stringify(Object.assign({}, { name: this.model.collection.name, conditions: this._conditions, fields: this._fields, o: this.options, populates: JSON.stringify(this._mongooseOptions.populate) })));
+        console.log(JSON.stringify(Object.assign({}, { name: this.model.collection.name, conditions: this._conditions, fields: this._fields, o: this.options, populates: JSON.stringify(this._mongooseOptions.populate) })))
 
         const cached = await client.get(key);
         if (cached) {
